96eed0f841fc20f2dd60ac28826855bc469f0537
Nagamalleswararao Ganji <nganji@codeaurora.org>
2012-04-29 22:39:48 -0700

    msm-fb: display: Reducing the display wake up time
    100 msec sleep in phy init delays system wake up. Using usleep instead of
    msleep to optimize it.
    
    Change-Id: Ibdbb771145210c9462aa68543024be2ae797be4f
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/msm_dss_io_8960.c

--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -486,16 +486,11 @@ void mipi_dsi_phy_init(int panel_ndx, struct msm_panel_info const *panel_info,
 	int i, off;
 
 	MIPI_OUTP(MIPI_DSI_BASE + 0x128, 0x0001);/* start phy sw reset */
-	/* LGE_CHANGE
-	 * Reduce delay time only for D1LV
-	 * 2012-04-06, baryun.hwang@lge.com
-	 */
-#ifdef CONFIG_MACH_MSM8960_D1LV
-	msleep(10);
-#else
-	msleep(100);
-#endif
+	wmb();
+	usleep(1);
 	MIPI_OUTP(MIPI_DSI_BASE + 0x128, 0x0000);/* end phy w reset */
+	wmb();
+	usleep(1);
 	MIPI_OUTP(MIPI_DSI_BASE + 0x500, 0x0003);/* regulator_ctrl_0 */
 	MIPI_OUTP(MIPI_DSI_BASE + 0x504, 0x0001);/* regulator_ctrl_1 */
 	MIPI_OUTP(MIPI_DSI_BASE + 0x508, 0x0001);/* regulator_ctrl_2 */
