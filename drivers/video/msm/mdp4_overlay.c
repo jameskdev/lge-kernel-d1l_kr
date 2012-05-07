e3521245e666daa7499e632f443def05e6d154da
Ravishangar Kalyanam <rkalya@codeaurora.org>
2012-05-07 12:02:41 -0700

    msm_fb: display: Flush pipe registers before staging the pipe
    Flush pipe registers before staging the pipe for the Mixer with
    pull mode panel interfaces. This fixes the IOMMU page fault/underrun
    caused by pipe fetching previously unmapped buffers due to missing flush
    calls
    
    CRs-Fixed: 356484
    Change-Id: I6bc2e49746a7c3773708796bc1709c75a8afaa4b
    Signed-off-by: Ravishangar Kalyanam <rkalya@codeaurora.org>
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay.c

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -2940,6 +2940,11 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		mdp4_overlay_rgb_setup(pipe);	/* rgb pipe */
 	}
 
+	if ((ctrl->panel_mode & MDP4_PANEL_DTV) ||
+		(ctrl->panel_mode & MDP4_PANEL_LCDC) ||
+		(ctrl->panel_mode & MDP4_PANEL_DSI_VIDEO))
+		mdp4_overlay_reg_flush(pipe, 0);
+
 	mdp4_mixer_stage_up(pipe);
 
 	if (pipe->mixer_num == MDP4_MIXER2) {
@@ -2954,7 +2959,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		ctrl->mixer1_played++;
 		/* enternal interface */
 		if (ctrl->panel_mode & MDP4_PANEL_DTV) {
-			mdp4_overlay_reg_flush(pipe, 0);
 			mdp4_overlay_dtv_start();
 			mdp4_overlay_dtv_ov_done_push(mfd, pipe);
 			if (!mfd->use_ov1_blt)
@@ -2965,7 +2969,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		/* primary interface */
 		ctrl->mixer0_played++;
 		if (ctrl->panel_mode & MDP4_PANEL_LCDC) {
-			mdp4_overlay_reg_flush(pipe, 0);
 			if (!mfd->use_ov0_blt)
 				mdp4_overlay_update_blt_mode(mfd);
 			mdp4_overlay_lcdc_start();
@@ -2973,7 +2976,6 @@ int mdp4_overlay_play(struct fb_info *info, struct msmfb_overlay_data *req)
 		}
 #ifdef CONFIG_FB_MSM_MIPI_DSI
 		else if (ctrl->panel_mode & MDP4_PANEL_DSI_VIDEO) {
-			mdp4_overlay_reg_flush(pipe, 0);
 
 			/* LGE_UPDATE_S, 2012-04-23, jamin.koo@lge.com
 			 * Displaying corrupted or blank image while playing only for HD/Full HD contents
