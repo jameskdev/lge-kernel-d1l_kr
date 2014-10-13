sonyxperiadev/kernel

msm: camera: Port camera v4l2 architecture to 8x60
Add V4L2 version files for vfe31 files for use in
V4L2 8x60 solution. Also, add option to use
correct platform devices for 8x60 if V4L2 flag is turned
on.

Change-Id: Ia209c2648122c13e3b4e14d4f93dae7f45f5492e
Signed-off-by: Kevin Chan <ktchan@codeaurora.org>
Signed-off-by: Ankit Premrajka <ankitp@codeaurora.org>

drivers/media/video/msm/sensors/s5k4e1_v4l2.c
@@ -452,7 +452,7 @@ static struct msm_sensor_fn_t s5k4e1_func_tbl = {
 	.sensor_set_fps = msm_sensor_set_fps,
 	.sensor_write_exp_gain = s5k4e1_write_prev_exp_gain,
 	.sensor_write_snapshot_exp_gain = s5k4e1_write_pict_exp_gain,
-	.sensor_setting = msm_sensor_setting1,
+	.sensor_csi_setting = msm_sensor_setting1,
 	.sensor_set_sensor_mode = msm_sensor_set_sensor_mode,
 	.sensor_mode_init = msm_sensor_mode_init,
 	.sensor_get_output_info = msm_sensor_get_output_info,

jameskdev/lge-kernel-d1l_kr

Signed-off-by: Roja Rani Yarubandi <rojay@codeaurora.org>
Conflicts:
	drivers/media/video/msm/Kconfig
	drivers/media/video/msm/Makefile
	drivers/media/video/msm/msm_mctl.c
	drivers/media/video/msm/sensors/s5k4e1_v4l2.c

drivers/media/video/msm/sensors/s5k4e1_v4l2.c
@@ -581,7 +581,7 @@ static struct msm_sensor_fn_t s5k4e1_func_tbl = {
 	.sensor_set_fps = s5k4e1_sensor_set_fps,
 	.sensor_write_exp_gain = s5k4e1_write_exp_gain1,
 	.sensor_write_snapshot_exp_gain = s5k4e1_write_exp_gain2,
-	.sensor_setting = msm_sensor_setting,
+	.sensor_csi_setting = msm_sensor_setting,
 	.sensor_set_sensor_mode = msm_sensor_set_sensor_mode,
 	.sensor_mode_init = msm_sensor_mode_init,
 	.sensor_get_output_info = msm_sensor_get_output_info,