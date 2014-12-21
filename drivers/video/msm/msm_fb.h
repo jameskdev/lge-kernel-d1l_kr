1d56099ca87f7703ce03ea662dc21b0f5e5bddeb
James Kim <grccorps@gmail.com>
2014-12-21 22:58:01 +0900

    Revert "msm_fb: display: Create sysfs entries in mdp_probe for MDP4 targets"
    This reverts commit 65a4a2b15e33bfb2583d4d1f664dd4ac5cb070ef.
    
    Conflicts:
    
    	drivers/video/msm/mdp4_overlay_dsi_cmd.c
    	drivers/video/msm/msm_fb.h

--- drivers/video/msm/msm_fb.h
+++ drivers/video/msm/msm_fb.h
@@ -130,7 +130,6 @@ struct msm_fb_data_type {
 	__u32 channel_irq;
 
 	struct mdp_dma_data *dma;
-	struct device_attribute dev_attr;
 	void (*dma_fnc) (struct msm_fb_data_type *mfd);
 	int (*cursor_update) (struct fb_info *info,
 			      struct fb_cursor *cursor);
@@ -141,8 +140,6 @@ struct msm_fb_data_type {
 	int (*start_histogram) (struct mdp_histogram_start_req *req);
 	int (*stop_histogram) (struct fb_info *info, uint32_t block);
 	void (*vsync_ctrl) (int enable);
-	void (*vsync_init) (int cndx);
-	void *vsync_show;
 	void *cursor_buf;
 	void *cursor_buf_phys;
 
@@ -210,7 +207,6 @@ struct msm_fb_data_type {
 	void *msm_fb_backup;
 	boolean panel_driver_on;
 	struct mutex entry_mutex;
-	int vsync_sysfs_created;
 	void *cpu_pm_hdl;
 };
 struct msm_fb_backup_type {
